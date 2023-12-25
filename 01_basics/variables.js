f37cdd4841dbebcea79de5005a1ae82c77127f82
bhavs28 <113660919+bhavs28@users.noreply.github.com>
2023-12-25 19:07:57 +0000

    variables basics

---
+++ 01_basics/variables.js
@@ -0,0 +1,21 @@
+const accountId=123;
+let accountName="BhavyaSri Kanchi";
+var accountPassword="Anu@bhavs1";
+acccountCity="Karimnagar";
+let email;
+
+console.table([accountId,accountName,accountPassword,acccountCity,email]);
+//accountId=345;//assignment to constant vriable error
+
+/*
+ Avoid using var
+ because of the block scope ..it can be changes even in blocks also
+ so frequenty use let for variable 
+ dont use variables without any key word also like accountcity in the example
+*/
+
+accountName="BhavyaSri";
+accountPassword="Anu1";
+acccountCity="Hyderabad";
+email="itzbhavyasri@gmail.com";
+console.table([accountId,accountName,accountPassword,acccountCity,email]);
