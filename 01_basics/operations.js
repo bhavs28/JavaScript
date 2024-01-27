fb0c4c4b2a927f80899ecb3fad5bd67db5295814
bhavs28 <113660919+bhavs28@users.noreply.github.com>
2024-01-27 14:03:17 +0000

    postfix and prefix

--- 01_basics/operations.js
+++ 01_basics/operations.js
@@ -9,10 +9,22 @@ let negvalue=-value;
 // console.log(2**3)
 // console.log(2%3)
 
-console.log(1+'2');
-console.log('1'+2 );
-console.log('1'+'1')
-console.log('1'+2+3)
-console.log(2+3+'1')
+// console.log(1+'2');
+// console.log('1'+2 );
+// console.log('1'+'1')
+// console.log('1'+2+3)
+// console.log(2+3+'1')
 
 console.log(+true)//not recommended coz confusing (o/p:1)
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
