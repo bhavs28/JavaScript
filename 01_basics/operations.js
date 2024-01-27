653ca7d1298c818cfa85097f74045ad621dada00
bhavs28 <113660919+bhavs28@users.noreply.github.com>
2024-01-27 14:00:28 +0000

    operarions

---
+++ 01_basics/operations.js
@@ -0,0 +1,18 @@
+let value=5
+let negvalue=-value;
+//console.log(negvalue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**3)
+// console.log(2%3)
+
+console.log(1+'2');
+console.log('1'+2 );
+console.log('1'+'1')
+console.log('1'+2+3)
+console.log(2+3+'1')
+
+console.log(+true)//not recommended coz confusing (o/p:1)
