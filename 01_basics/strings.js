2492c657ce2efaa4b8407241f38add527c9b2fab
bhavs28 <113660919+bhavs28@users.noreply.github.com>
2024-01-29 14:29:54 +0000

    strings and math

---
+++ 01_basics/strings.js
@@ -0,0 +1,6 @@
+const name="Bhavyasri Kanchi"
+const age=20;
+
+//console.log("My name is "+name + " and I am "+age+ "years old");
+
+console.log(`My name is ${name} and I am ${age} years old`)//This is called string Interpolation
