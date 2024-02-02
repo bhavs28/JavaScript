ea0f21c515f26e490d91b7f14a1e31cfcb9fc9d3
bhavs28 <113660919+bhavs28@users.noreply.github.com>
2024-02-02 06:56:43 +0000

    arrays methods

--- 02_basics/arrays.js
+++ 02_basics/arrays.js
@@ -1,14 +1,34 @@
 const tvd=['vampires','warewolves','witches','originals','hybrids']
 console.log(tvd)
 
-// Constructor function
-function Person(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-  
-  // Creating an instance of Person
-  var person1 = new Person('John', 30);
-  
-  // Accessing the prototype of the object
-  console.log(Person.prototype);
+// tvd.push("klaus")
+// console.log(tvd)
+
+// tvd.pop()
+// console.log(tvd)
+
+// tvd.unshift("season 1")
+// console.log(tvd)
+// tvd.shift()
+// console.log(tvd)
+
+// console.log(tvd.includes("hybrids"));
+// console.log(tvd.indexOf("witches"))
+
+//console.log(tvd.join());
+
+// console.log(tvd.slice(1,3))
+// console.log(tvd)
+
+//console.log(tvd.splice(1,3));
+//console.log(tvd)
+
+//const arr=[1,2,3,[3,4],["stefan","damon",[5,6,7]],8,9]
+//console.log(arr.flat(Infinity));
+//console.log(tvd.concat(arr.flat(3)));
+
+//console.log(Array.from("Bhavyasri"))
+
+const a=100;
+const b=200,c=300;
+console.log(Array.of(a,b,c));
