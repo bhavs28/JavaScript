75d215ef24ecf97bc7791c14f8554470bf8b5208
bhavs28 <113660919+bhavs28@users.noreply.github.com>
2024-02-06 06:20:14 +0000

    arrow fn basics

--- 02_basics/arrow.js
+++ 02_basics/arrow.js
@@ -11,15 +11,27 @@ user.welcomemsg();
 user.name='anunya'
 user.welcomemsg()
 
-console.log(this);//gives an empty obj but in browser console it will be window object
+//console.log(this);//gives an empty obj but in browser console it will be window object
 
 // function showry(){
 //     console.log(this);//something is there in 'this'
 // }
 // showry()
 
-// function showry(){
-//     const user='shahriyaar'
-//     console.log(this.user);//gives undefined
-// }
-// showry()
+const chai=function(){
+    username='bhavs'
+    //console.log(this);
+    console.log(this.username);
+}
+chai()
+
+const arrowfun=()=>{
+    username='kanchi';
+    console.log(this.username);
+}
+arrowfun()
+
+const add=(num1,num2)=> num1+num2;//implicit return
+const add1=(num1)=>(num1+1)//using brackets ..another way
+//eg:- ({dsakjd:bdkcsdh})without () it wont return the obj
+console.log(add1(2));
